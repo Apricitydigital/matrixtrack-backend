b52c5f457eb48fc12eb337edb62056ecbe5ae98b
Ashutosh Parakh <ashu70056@gmail.com>
2026-02-27 18:10:43 +0530

    Updated cors for portal.matrixtrack.in

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const defaultOrigins = [
   "http://10.205.83.56:8081",
   "http://10.205.83.56:8082",
   "http://10.205.83.56:19000",
-  "https://app.matrixtrack.in",
+  "https://portal.matrixtrack.in",
 ];
 
 const parseOrigins = (value) =>
