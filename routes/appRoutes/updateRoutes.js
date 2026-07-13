b0f9b1fe46e4b7f20743d742aebc722c9ca85f1f
Ashutosh Parakh <ashu70056@gmail.com>
2026-07-13 11:31:22 +0530

    remove force update warning

--- routes/appRoutes/updateRoutes.js
+++ routes/appRoutes/updateRoutes.js
@@ -9,8 +9,8 @@ router.get("/version", (req, res) => {
   // In a real scenario, these could be stored in a database or env variables
   const versionConfig = {
     latestVersion: "1.0.9",
-    minVersion: "1.0.9", // If app version is less than this, force update
-    forceUpdate: true,
+    minVersion: "1.0.0", // Disable force update for older versions by setting to 1.0.0
+    forceUpdate: false,  // Set to false to prevent blocking customers
     updateUrl: "https://play.google.com/store/apps/details?id=com.apricitydigital.attendeaseApp",
     message: "A new version of MatrixTrack is available. Please update to continue using the application."
   };
